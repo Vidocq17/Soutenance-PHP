e914242abddb8c41ba8848707d8179dcf81011c6
Vidocq17 <AvidanB1704@gmail.com>
2023-05-28 12:16:56 +0200

    modif css inscription

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <html>
     <link rel="stylesheet" href="index.css">
 <body>
-    <div class="left-panel">
-    <h1>Bienvenue sur WeTchat !</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis officia similique sequi voluptatum placeat eum architecto perspiciatis! Molestias ipsa, repellendus illum fuga amet, nesciunt officiis repudiandae voluptas ut dolorem unde.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis officia similique sequi voluptatum placeat eum architecto perspiciatis! Molestias ipsa, repellendus illum fuga amet, nesciunt officiis repudiandae voluptas ut dolorem unde.</p>
-</div>
+        <div class="title">
+            <h1>Bienvenue sur WeTchat !</h1>
+        </div>
+    <p>Notre nouveau réseau social, nommé "WeTchat", est une plateforme en ligne innovante qui vise à rapprocher les personnes du monde entier et à favoriser la connectivité sociale. <br>  <br>
+    WeTchat offre un espace virtuel convivial où les utilisateurs peuvent interagir, partager des informations, établir des relations et explorer de nouveaux horizons. <br> <br>
+    Caractéristiques clés de WeTchat : <br><br>
+    1. Profils personnalisés : Les utilisateurs peuvent créer des profils personnalisés en ajoutant des informations telles que leur nom, leur photo, leur localisation, leurs intérêts, etc. Ils peuvent également choisir leurs préférences de confidentialité pour contrôler les informations partagées. <br><br>
+    2. Flux d'actualités : Le flux d'actualités de WeTchat est le cœur du réseau social, où les utilisateurs peuvent voir les publications de leurs amis, des pages qu'ils suivent et des communautés auxquelles ils sont membres. Le flux d'actualités est alimenté par des algorithmes intelligents qui affichent des contenus pertinents et intéressants. <br><br>
+    3. Publications multimédias : Les utilisateurs peuvent partager des messages, des photos, des vidéos et des liens avec leurs amis et leurs abonnés. Ils ont également la possibilité de commenter, d'aimer et de partager les publications des autres utilisateurs.<br> <br>
+    4. Messagerie instantanée : WeTchat propose une fonction de messagerie instantanée permettant aux utilisateurs d'envoyer des messages privés, de créer des groupes de discussion et de partager des fichiers avec leurs contacts. <br><br>
+    5. Groupes et communautés : Les utilisateurs peuvent rejoindre des groupes et des communautés basés sur leurs centres d'intérêt, leurs hobbies ou leurs affinités. Cela leur permet de rencontrer des personnes partageant les mêmes intérêts et d'interagir avec elles. <br><br>
+    6. Fonctionnalités de confidentialité : WeTchat met l'accent sur la protection de la vie privée de ses utilisateurs. Les utilisateurs ont un contrôle total sur leurs paramètres de confidentialité et peuvent choisir qui peut voir leurs publications, leurs informations personnelles, etc. <br><br>
+    7. Exploration et découverte : WeTchat offre des fonctionnalités de recherche avancées et de découverte de contenus pour aider les utilisateurs à trouver de nouvelles personnes intéressantes à suivre, des pages à aimer et des communautés à rejoindre. <br>
+    8. Applications mobiles : WeTchat est disponible sur les plateformes mobiles, offrant une expérience fluide et optimisée pour les utilisateurs de smartphones et de tablettes. WeTchat vise à créer une expérience sociale positive en encourageant des interactions authentiques, le partage d'idées et la création de relations significatives. <br><br>
+    Rejoignez-nous sur ConnectX et explorez un nouveau monde de connectivité sociale en ligne.</p>
 <div class="right-panel">
     <a href="inscription.php"><button class="btn"> je m'inscris</button></a>
     <a href="connexion.php"><button class="btn"> je me connecte</button></a>
--- inscription.php
+++ inscription.php
@@ -65,7 +65,6 @@ if (isset($_POST['register'])) {
 ?>
 
 
-?>
 
 <!DOCTYPE html>
 <html>
